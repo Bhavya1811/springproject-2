7429285c6d7457cf6f02260f5d9e9fc9850ef053
Bhavya1811 <bhavyapeddi123@gmail.com>
2021-07-11 23:58:28 -0500

    springproject-2
    springproject-2

---
+++ src/main/java/com/assignment/springproject2/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.assignment.springproject2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.assignment.springproject2.entity.Product;
+import com.assignment.springproject2.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public Product saveProduct(Product product) {
+		return repository.save(product);
+	}
+
+	public List<Product> saveProducts(List<Product> products) {
+		return repository.saveAll(products);
+	}
+
+	public List<Product> getProducts() {
+		return repository.findAll();
+	}
+
+	public Product getProductById(int id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public Product getProductByName(String name) {
+		return repository.findByName(name);
+	}
+
+	public String deleteProduct(int id) {
+		repository.deleteById(id);
+		return "product removed" + id;
+	}
+
+	public Product updateProduct(Product product) {
+		Product existingProduct = repository.findById(product.getId()).orElse(null);
+		existingProduct.setName(product.getName());
+		existingProduct.setPrice(product.getPrice());
+		existingProduct.setQuantity(product.getQuantity());
+		return repository.save(existingProduct);
+	}
+
+}
